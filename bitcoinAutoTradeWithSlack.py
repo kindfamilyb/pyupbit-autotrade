b4fb71cd5fda4ba2690cdf47d330082d3ace371f
DESKTOP-R0QD6K2\woody <kindfamily@kakao.com>
2022-10-15 21:30:06 +0900

    타겟가격, 거래시작시간 계산을 23:59:24로 변경

--- bitcoinAutoTradeWithSlack.py
+++ bitcoinAutoTradeWithSlack.py
@@ -23,13 +23,13 @@ def post_message(token, channel, text):
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    df = pyupbit.get_daily_ohlcv_from_base(ticker="KRW-BTC", base=23.99)[-2:]
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_start_time(ticker):
     """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    df = pyupbit.get_daily_ohlcv_from_base(ticker="KRW-BTC", base=23.99)[-1:]
     start_time = df.index[0]
     return start_time
 
